d4d8d4504868143268d5321bec1a490de9cb8ce6
Mina <MinaNgh@users.noreply.github.com>
2018-12-18 14:02:11 -0800

    Update

--- server.js
+++ server.js
@@ -1,7 +1,7 @@
 const express = require("express");
 const app = express();
 app.get("/", (req, res)=>{
-    res.sendFile(__dirname+'/view')
+    res.sendFile(__dirname+'/view/index.html')
 });
 app.use("/", express.static(__dirname+'/public'));
 app.listen(process.env.PORT || 8080);
