ea9257a1de507e19ea84fa7253001efaf40cbbd3
Mina <MinaNgh@users.noreply.github.com>
2018-12-18 16:36:10 -0800

    Update

--- server.js
+++ server.js
@@ -3,12 +3,16 @@ const app = express();
 app.get("/", (req, res)=>{
     res.sendFile(__dirname+'/view/index.html')
 });
-app.use("/", express.static(__dirname+'/public/style.css'));
-app.get("/:time",(req, res)=>{
+app.use("/", express.static(__dirname+'/public'));
+app.get("/api/timestamp/:time",(req, res)=>{
 	var str = req.params.time;
 	if(str !==""){
+		
 		res.json({"unix":0,"utc":0});
-	}else	
-		res.json({"unix":10,"utc":10});
+	}else
+		var date = new Date();
+		var unix = date.getTime();
+		var utc = date.toUTCString();	
+		res.json({"unix":unix,"utc":utc});
 });
 app.listen(process.env.PORT || 8080);
