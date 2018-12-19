6144228fbe1fa0f4cdb4104af7a923793e547a5a
Mina <MinaNgh@users.noreply.github.com>
2018-12-18 16:37:45 -0800

    Update

--- server.js
+++ server.js
@@ -9,10 +9,11 @@ app.get("/api/timestamp/:time",(req, res)=>{
 	if(str !==""){
 		
 		res.json({"unix":0,"utc":0});
-	}else
+	}else{
 		var date = new Date();
 		var unix = date.getTime();
 		var utc = date.toUTCString();	
 		res.json({"unix":unix,"utc":utc});
+	}
 });
 app.listen(process.env.PORT || 8080);
