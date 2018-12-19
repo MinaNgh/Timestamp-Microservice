285c241879014a3eb9e4ae9fc3cbeaa09c76c327
Mina <MinaNgh@users.noreply.github.com>
2018-12-19 10:46:37 -0800

    Update

--- server.js
+++ server.js
@@ -1,19 +1,14 @@
 const express = require("express");
+const parseTime = require("./parseTime.js");
 const app = express();
-app.get("/", (req, res)=>{
-    res.sendFile(__dirname+'/view/index.html')
+console.log(__dirname);
+app.use(express.static(__dirname+'/public'));
+app.get('/',(req, res)=>{
+  res.sendFile(__dirname+'/view/index.html');
 });
-app.use("/", express.static(__dirname+'/public'));
 app.get("/api/timestamp/:time",(req, res)=>{
-	var str = req.params.time;
-	if(str !==""){
-		
-		res.json({"unix":0,"utc":0});
-	}else{
-		var date = new Date();
-		var unix = date.getTime();
-		var utc = date.toUTCString();	
-		res.json({"unix":unix,"utc":utc});
-	}
+	var value = req.params.time;
+	res.json(parseTime(value));
+	// res.json({"utc": new Date(Number(str))})
 });
 app.listen(process.env.PORT || 8080);
